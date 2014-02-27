3dcc03c41a5b0c096176cb58ec6af8f8c2f44a49
Josh Glover <jmglov@gmail.com>
2014-02-27 16:31:01 +0100

    project.clj : add description, URL

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject kais-mario-cart "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "My son's first video game"
+  :url "https://github.com/jmglov/kais-mario-cart"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main kais-mario-cart.core)
